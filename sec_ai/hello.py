1f75865b6686a46b08bef6963d361fe8d74c37f5
Alphanome.AI <137115920+alphanome@users.noreply.github.com>
2023-11-19 05:28:32 +0200

    Update hello.py

--- sec_ai/hello.py
+++ sec_ai/hello.py
@@ -1,2 +1,6 @@
 def hello() -> str:
     return "Hello, world!"
+# Check out 
+#    https://github.com/alphanome-ai/sec-parser
+# where the majority of the development is happening right now.
+# We'll get back to sec-ai once sec-parser is more ready.
