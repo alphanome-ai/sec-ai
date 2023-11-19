eb1d2ed2a2b2fcbc5a67d56287ec091bf0efc28f
Elijas <4084885+Elijas@users.noreply.github.com>
2023-11-19 05:30:53 +0200

    chore: fix lint

--- sec_ai/hello.py
+++ sec_ai/hello.py
@@ -1,6 +1,6 @@
 def hello() -> str:
     return "Hello, world!"
-# Check out 
+# Check out
 #    https://github.com/alphanome-ai/sec-parser
 # where the majority of the development is happening right now.
 # We'll get back to sec-ai once sec-parser is more ready.
